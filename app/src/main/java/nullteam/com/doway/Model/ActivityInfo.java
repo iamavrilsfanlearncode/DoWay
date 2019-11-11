ad51bcd6d25208535fee60735309654e464102d3
WenCheng <iamavrilsfany9x@gmail.com>
2019-11-11 20:31:15 +0800

    add Model   Restaurant -> 農村美食   Hotel -> 民宿   ActivityInfo -> 觀光活動

---
+++ app/src/main/java/nullteam/com/doway/Model/ActivityInfo.java
@@ -0,0 +1,280 @@
+package nullteam.com.doway.Model;
+
+public class ActivityInfo {
+    /**
+     * Id : C2_315080000H_019960
+     * Name : 2019新北市萬金石馬拉松
+     * Description : 「新北市萬金石馬拉松」歷史可追朔至2003年的「金山馬拉松」，舉辦至2018年將邁入第16屆，首屆賽事起終點設置於金山青年活動中心，是一場僅約4,000人參加的地方性小型體育賽事，經過多年的品牌經營，成為全臺唯一國際田徑總會認證的標籤賽事。2004年，賽事路線擴張為金山及石門兩個行政區域，並將賽事名稱更改為「金石馬拉松」，後來為減少賽事交通管制影響地方居民，2006年將起終點改設置於萬里翡翠灣，歷經5年醞釀，2009年賽事路線擴張為萬里、金山及石門等三個行政區域，並將行政區域地名列入賽事名稱，賽事再度更名為「石萬金馬拉松」。2010年，為順應賽道行經路線順序，由萬里翡翠灣起跑，經金山至石門國中前折返之賽道路線，賽事名稱正式為「萬金石馬拉松」。至此，奠定賽事籌備行政區域規模、賽道路線以及賽事名稱，經多年賽事宣傳經營，漸漸形成「賽事品牌」概念，逐年屢創參賽人數新高，吸引眾多跑友踴躍報名參賽，並在2011年激增報名人數3000多人，總參賽人數超過9000人。2012年參賽人數正式突破萬人，成為臺灣炙手可熱的馬拉松賽事。2013年，第11屆賽事順利舉行完畢後，國內專家學者表示全臺僅「萬金石馬拉松」有條件申請國際田徑總會（International Association of Athletics Federations，簡稱IAAF）路跑標籤認證，新北市於第118次市政會議，由朱立倫市長宣布萬金石馬拉松積極爭取IAAF認證，由新北市政府體育處負責籌備相關申請認證工作，首先以通過「銅標籤BRONZE」認證為目標。2014年萬金石馬拉松，正式委由中華民國田徑協會向國際田徑總會提出「銅標籤」認證申請，並且順利於同年10月正式由國際田徑總會宣布通過認證，「新北市萬金石馬拉松」成為臺灣首場且唯一的IAAF銅標籤賽事，不僅成為臺灣最具代表性馬拉松賽事之一，並登錄IAAF年度馬拉松行事曆，讓新北市成功躍上世界體育舞臺，當年賽事除申請國際認證足為稱道外，萬里、金山及石門區公所號召超過千人之在地團體組成「啦啦隊加油團」，賽事當日凌晨即在賽道沿線盛裝打扮，融入如萬里蟹、女王頭、金山溫泉及石門風箏等地方特色物產，更有大鼓表演及電音三太子等傳統民俗，不畏清晨低溫散發全身熱力為每位選手打氣，更令眾人感受到在地熱情氛圍，賽事已經跨越交通管制的不便障礙，凝聚地方居民共識，成為北海岸在地居民認同且支持的年度盛會。2015年，是萬金石馬拉松通過IAAF銅標籤認證後第一場賽事，考量北海岸地區3月初氣候多雨濕冷，將以往每年固定於3月第1個星期天舉行之賽事慣例，調整為3月第3個星期天，將有12,000位愛好馬拉松賽事選手，以及眾多國際菁英選手同場較勁，大會更開放試辦外籍選手homestay體驗行程，服務來臺參加萬金石馬拉松的外籍選手，安排兩天三夜的家庭接待住宿，規劃野柳地質奇景及漁港文化、在地歷史民居建築及金山八煙聚落參訪課程，讓馬拉松不只是體育競賽，而是一種體驗在地特色生活文化的契機。2017年萬金石馬拉松首次與文創結合，除了邀請現代新銳設計師方序中設計選手跑衣，並邀請國內知名插畫家喜花如、鬼門圖文、辛卡米克等設計周邊，另與朱銘美術館合作活動現場拍照裝置，並由BSM Design一手打造2017萬金石主視覺與限定周邊商品，讓萬金石馬拉松不僅是一項馬拉松盛會，同時結合在地觀光、藝術及文創等，讓運動不再只運動，也可以很藝術很時尚。「2018新北市萬金石馬拉松」，訂於2018年3月18日清晨6時30分，在萬里翡翠灣鳴槍起跑，持續以「#only WAN」獨一無二、萬中選一之品牌精神貫穿賽事，不僅是全臺唯一IAAF國際認證馬拉松，更是跑界中一場絕美的視覺饗宴，山海交錯的美麗維納斯海岸線盡收眼底。新北市萬金石馬拉松於2017年12月通過國際田徑總會（IAAF）「銀標籤（SILVER）」認證，成為臺灣首場也是唯一通過國際認證之馬拉松賽事！2019新北市萬金石馬拉已將邁入第17屆，於3月17日舉辦，持續延伸賽事的主題「#only WAN」的精神與目標及符合第17屆的意涵，打造更具話題性、可看性的主軸-「#17來WAN」。
+     * Participation :
+     * Location : 新北市 萬里區
+     * Add : 海景路1號
+     * Region : 新北市
+     * Town : 萬里區
+     * Tel :
+     * Org : 新北市政府、中華民國田徑協會
+     * Start : 2019-03-17T00:00:00+08:00
+     * End : 2019-03-17T00:00:00+08:00
+     * Cycle :
+     * Noncycle :
+     * Website :
+     * Picture1 : https://www.taiwan.net.tw/att/event/c9810517-ad09-4e21-8407-4e0564659ca6.jpg
+     * Picdescribe1 : 全臺最美麗的「維納斯海岸線」
+     * Picture2 : https://www.taiwan.net.tw/att/event/2cc79f48-4c41-4619-9e04-5b743ad3f554.jpg
+     * Picdescribe2 : 萬金石馬拉松跑者融入山海美景中
+     * Picture3 : https://www.taiwan.net.tw/att/event/6291cd8c-db0c-4c02-b9a2-daa8eb70f351.jpg
+     * Picdescribe3 : 萬金石在地景觀專屬的龜吼駱駝峰跑線
+     * Px : 121.683852
+     * Py : 25.19107
+     * Class1 : 01
+     * Class2 : 02
+     * Map :
+     * Travellinginfo :
+     * Parkinginfo :
+     * Charge :
+     * Remarks :
+     */
+
+    private String Id;
+    private String Name;
+    private String Description;
+    private String Participation;
+    private String Location;
+    private String Add;
+    private String Region;
+    private String Town;
+    private String Tel;
+    private String Org;
+    private String Start;
+    private String End;
+    private String Cycle;
+    private String Noncycle;
+    private String Website;
+    private String Picture1;
+    private String Picdescribe1;
+    private String Picture2;
+    private String Picdescribe2;
+    private String Picture3;
+    private String Picdescribe3;
+    private double Px;
+    private double Py;
+    private String Travellinginfo;
+    private String Parkinginfo;
+    private String Charge;
+    private String Remarks;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getParticipation() {
+        return Participation;
+    }
+
+    public void setParticipation(String Participation) {
+        this.Participation = Participation;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String Location) {
+        this.Location = Location;
+    }
+
+    public String getAdd() {
+        return Add;
+    }
+
+    public void setAdd(String Add) {
+        this.Add = Add;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String Region) {
+        this.Region = Region;
+    }
+
+    public String getTown() {
+        return Town;
+    }
+
+    public void setTown(String Town) {
+        this.Town = Town;
+    }
+
+    public String getTel() {
+        return Tel;
+    }
+
+    public void setTel(String Tel) {
+        this.Tel = Tel;
+    }
+
+    public String getOrg() {
+        return Org;
+    }
+
+    public void setOrg(String Org) {
+        this.Org = Org;
+    }
+
+    public String getStart() {
+        return Start;
+    }
+
+    public void setStart(String Start) {
+        this.Start = Start;
+    }
+
+    public String getEnd() {
+        return End;
+    }
+
+    public void setEnd(String End) {
+        this.End = End;
+    }
+
+    public String getCycle() {
+        return Cycle;
+    }
+
+    public void setCycle(String Cycle) {
+        this.Cycle = Cycle;
+    }
+
+    public String getNoncycle() {
+        return Noncycle;
+    }
+
+    public void setNoncycle(String Noncycle) {
+        this.Noncycle = Noncycle;
+    }
+
+    public String getWebsite() {
+        return Website;
+    }
+
+    public void setWebsite(String Website) {
+        this.Website = Website;
+    }
+
+    public String getPicture1() {
+        return Picture1;
+    }
+
+    public void setPicture1(String Picture1) {
+        this.Picture1 = Picture1;
+    }
+
+    public String getPicdescribe1() {
+        return Picdescribe1;
+    }
+
+    public void setPicdescribe1(String Picdescribe1) {
+        this.Picdescribe1 = Picdescribe1;
+    }
+
+    public String getPicture2() {
+        return Picture2;
+    }
+
+    public void setPicture2(String Picture2) {
+        this.Picture2 = Picture2;
+    }
+
+    public String getPicdescribe2() {
+        return Picdescribe2;
+    }
+
+    public void setPicdescribe2(String Picdescribe2) {
+        this.Picdescribe2 = Picdescribe2;
+    }
+
+    public String getPicture3() {
+        return Picture3;
+    }
+
+    public void setPicture3(String Picture3) {
+        this.Picture3 = Picture3;
+    }
+
+    public String getPicdescribe3() {
+        return Picdescribe3;
+    }
+
+    public void setPicdescribe3(String Picdescribe3) {
+        this.Picdescribe3 = Picdescribe3;
+    }
+
+    public double getPx() {
+        return Px;
+    }
+
+    public void setPx(double Px) {
+        this.Px = Px;
+    }
+
+    public double getPy() {
+        return Py;
+    }
+
+    public void setPy(double Py) {
+        this.Py = Py;
+    }
+
+    public String getTravellinginfo() {
+        return Travellinginfo;
+    }
+
+    public void setTravellinginfo(String Travellinginfo) {
+        this.Travellinginfo = Travellinginfo;
+    }
+
+    public String getParkinginfo() {
+        return Parkinginfo;
+    }
+
+    public void setParkinginfo(String Parkinginfo) {
+        this.Parkinginfo = Parkinginfo;
+    }
+
+    public String getCharge() {
+        return Charge;
+    }
+
+    public void setCharge(String Charge) {
+        this.Charge = Charge;
+    }
+
+    public String getRemarks() {
+        return Remarks;
+    }
+
+    public void setRemarks(String Remarks) {
+        this.Remarks = Remarks;
+    }
+}
---
+++ app/src/main/java/nullteam/com/doway/Model/Hotel.java
@@ -0,0 +1,248 @@
+package nullteam.com.doway.Model;
+
+public class Hotel {
+    /**
+     * Id : C4_315080000H_000008
+     * Name : 思源居民宿
+     * Description : 位於南投縣的民宿
+     * Grade : 0
+     * Add : 南投縣埔里鎮水頭里水頭路1號
+     * Zipcode : 545
+     * Region : 南投縣
+     * Town : 埔里鎮
+     * Tel : 886-49-2927101
+     * Fax : 886--
+     * Gov : 315080000H
+     * Website :
+     * Picture1 : https://taiwan.taiwanstay.net.tw/twpic/15545.jpg
+     * Picdescribe1 : 外觀
+     * Picture2 :
+     * Picdescribe2 :
+     * Picture3 :
+     * Picdescribe3 :
+     * Px : 120.970365
+     * Py : 23.935199
+     * Class : 4
+     * Map :
+     * Spec :
+     * Serviceinfo :
+     * Parkinginfo : 車位:小客車0輛、機車0輛、大客車0輛
+     */
+
+    private String Id;
+    private String Name;
+    private String Description;
+    private String Grade;
+    private String Add;
+    private String Zipcode;
+    private String Region;
+    private String Town;
+    private String Tel;
+    private String Fax;
+    private String Gov;
+    private String Website;
+    private String Picture1;
+    private String Picdescribe1;
+    private String Picture2;
+    private String Picdescribe2;
+    private String Picture3;
+    private String Picdescribe3;
+    private double Px;
+    private double Py;
+    private String Map;
+    private String Spec;
+    private String Serviceinfo;
+    private String Parkinginfo;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(String Grade) {
+        this.Grade = Grade;
+    }
+
+    public String getAdd() {
+        return Add;
+    }
+
+    public void setAdd(String Add) {
+        this.Add = Add;
+    }
+
+    public String getZipcode() {
+        return Zipcode;
+    }
+
+    public void setZipcode(String Zipcode) {
+        this.Zipcode = Zipcode;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String Region) {
+        this.Region = Region;
+    }
+
+    public String getTown() {
+        return Town;
+    }
+
+    public void setTown(String Town) {
+        this.Town = Town;
+    }
+
+    public String getTel() {
+        return Tel;
+    }
+
+    public void setTel(String Tel) {
+        this.Tel = Tel;
+    }
+
+    public String getFax() {
+        return Fax;
+    }
+
+    public void setFax(String Fax) {
+        this.Fax = Fax;
+    }
+
+    public String getGov() {
+        return Gov;
+    }
+
+    public void setGov(String Gov) {
+        this.Gov = Gov;
+    }
+
+    public String getWebsite() {
+        return Website;
+    }
+
+    public void setWebsite(String Website) {
+        this.Website = Website;
+    }
+
+    public String getPicture1() {
+        return Picture1;
+    }
+
+    public void setPicture1(String Picture1) {
+        this.Picture1 = Picture1;
+    }
+
+    public String getPicdescribe1() {
+        return Picdescribe1;
+    }
+
+    public void setPicdescribe1(String Picdescribe1) {
+        this.Picdescribe1 = Picdescribe1;
+    }
+
+    public String getPicture2() {
+        return Picture2;
+    }
+
+    public void setPicture2(String Picture2) {
+        this.Picture2 = Picture2;
+    }
+
+    public String getPicdescribe2() {
+        return Picdescribe2;
+    }
+
+    public void setPicdescribe2(String Picdescribe2) {
+        this.Picdescribe2 = Picdescribe2;
+    }
+
+    public String getPicture3() {
+        return Picture3;
+    }
+
+    public void setPicture3(String Picture3) {
+        this.Picture3 = Picture3;
+    }
+
+    public String getPicdescribe3() {
+        return Picdescribe3;
+    }
+
+    public void setPicdescribe3(String Picdescribe3) {
+        this.Picdescribe3 = Picdescribe3;
+    }
+
+    public double getPx() {
+        return Px;
+    }
+
+    public void setPx(double Px) {
+        this.Px = Px;
+    }
+
+    public double getPy() {
+        return Py;
+    }
+
+    public void setPy(double Py) {
+        this.Py = Py;
+    }
+    
+    public String getMap() {
+        return Map;
+    }
+
+    public void setMap(String Map) {
+        this.Map = Map;
+    }
+
+    public String getSpec() {
+        return Spec;
+    }
+
+    public void setSpec(String Spec) {
+        this.Spec = Spec;
+    }
+
+    public String getServiceinfo() {
+        return Serviceinfo;
+    }
+
+    public void setServiceinfo(String Serviceinfo) {
+        this.Serviceinfo = Serviceinfo;
+    }
+
+    public String getParkinginfo() {
+        return Parkinginfo;
+    }
+
+    public void setParkinginfo(String Parkinginfo) {
+        this.Parkinginfo = Parkinginfo;
+    }
+}

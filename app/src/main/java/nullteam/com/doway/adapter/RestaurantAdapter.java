86f276ee6751cc6c182b58fc6fa85e0b09d64795
GeorgeTseng <GeorgeMomCL@gmail.com>
2019-11-19 11:10:47 +0800

    完成列表預設用圖示
    純色圓框ImageView+文字覆蓋於上

--- app/src/main/java/nullteam/com/doway/adapter/RestaurantAdapter.java
+++ app/src/main/java/nullteam/com/doway/adapter/RestaurantAdapter.java
@@ -45,67 +45,9 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Vi
         holder.addressTextView.setText(datas.get(position).getAddress());
         holder.telTextView.setText(datas.get(position).getTel());
         holder.restaurant = datas.get(position);
-        //用getCity()方法讀取每筆資料的縣市
-        if (datas.get(position).getCity().equals("基隆市")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_blue);
-        }
-        if (datas.get(position).getCity().equals("新北市")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_cyan);
-        }
-        if(datas.get(position).getCity().equals("臺北市")){
-            holder.picImageView.setImageResource(R.drawable.fullcolor_green);
-        }
-        if (datas.get(position).getCity().equals("桃園市")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_purple);
-        }
-        if (datas.get(position).getCity().equals("新竹縣")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_red);
-        }
-        if (datas.get(position).getCity().equals("新竹市")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_yellow);
-        }
-        if (datas.get(position).getCity().equals("苗栗縣")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor);
-        }
-        if(datas.get(position).getCity().equals("臺中市")){
-            holder.picImageView.setImageResource(R.drawable.fullcolor_reverse);
-        }
-        if (datas.get(position).getCity().equals("彰化縣")) {
-            holder.picImageView.setImageResource(R.drawable.movieicon1);
-        }
-        if (datas.get(position).getCity().equals("雲林縣")) {
-            holder.picImageView.setImageResource(R.drawable.baseball_icon);
-        }
-        if (datas.get(position).getCity().equals("嘉義縣")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_high);
-        }
-        if(datas.get(position).getCity().equals("臺南市")){
-            holder.picImageView.setImageResource(R.drawable.fullcolor_low);
-        }
-        if (datas.get(position).getCity().equals("高雄市")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_mid);
-        }
-        if (datas.get(position).getCity().equals("屏東縣")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_high_high);
-        }
-        if (datas.get(position).getCity().equals("宜蘭縣")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_high_low);
-        }
-        if (datas.get(position).getCity().equals("花蓮縣")) {
-            holder.picImageView.setImageResource(R.drawable.fullcolor_low_high);
-        }
-        if(datas.get(position).getCity().equals("臺東縣")){
-            holder.picImageView.setImageResource(R.drawable.fullcolor_low_low);
-        }
-        if (datas.get(position).getCity().equals("南投縣")) {
-            holder.picImageView.setImageResource(R.drawable.bible_icon);
-        }
-        if (datas.get(position).getCity().equals("連江縣")) {
-            holder.picImageView.setImageResource(R.drawable.storage_icon_2);
-        }
-		if (datas.get(position).getCity().equals("金門縣")){
-            holder.picImageView.setImageResource(R.drawable.dbicon1);
-        }
+        //用getCity()方法取得每筆資料的縣市
+        holder.cityTextView.setText(datas.get(position).getCity());
+        //設定預設圖
     }
 
     @Override

b6f403a2af5bb699d927d08ff2a4cb69fdc3bee1
LulaKao <windandsprings@gmail.com>
2019-11-26 17:47:21 +0800

    新增 about 的 fragment

--- app/src/main/java/nullteam/com/doway/adapter/ActivityInfoAdapter.java
+++ app/src/main/java/nullteam/com/doway/adapter/ActivityInfoAdapter.java
@@ -7,13 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
-
 import nullteam.com.doway.R;
 import nullteam.com.doway.activity.ActivityInfoDetail;
 import nullteam.com.doway.model.ActivityInfo;
